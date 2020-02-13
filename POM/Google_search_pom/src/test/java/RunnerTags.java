3fb5b3f1c711a49d2a2ed03eecffd6a64de5c6cc
test monedero <test monedero>
2020-02-13 14:40:01 -0500

    new project

---
+++ POM/Google_search_pom/src/test/java/RunnerTags.java
@@ -0,0 +1,12 @@
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions (features = "src/test/resources/features/google_search.feature", tags = "@searchButton")
+//@CucumberOptions (features = "src/test/resources/features/google_search.feature", tags = "@allcase")
+public class RunnerTags {
+
+}
